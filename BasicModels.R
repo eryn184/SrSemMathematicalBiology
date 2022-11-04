92edead5869f348922defe4c8bed1781b5c5ca2f
eryn184 <tucker.eryn.nicole@gmail.com>
2022-11-04 13:13:10 -0500

    test function

--- BasicModels.R
+++ BasicModels.R
@@ -21,8 +21,21 @@ model1 <- function(r, t) {
 #dy/dt = r y(t) + sin(t) y(t)
 
 
-euler(dy.dt=function(t,y){(.118849*y) + ((.118849/1100000)*y^2)}, .1, 253000)
+results <- euler(dy.dt=function(t,y){(.118849*y) - sin(y) + ((.118849/1100000)*y^2)}, .1, 253000, 0, 10)
+
+results <- data.frame(results)
+
+ggplot(results, mapping = aes(x = 1:101, y = results)) +
+  geom_point()
 
 
+
+#test
+
+results <- euler(dy.dt=function(t,y){(cos(t)-y)/(t+1)}, .01, 0, 0, 10)
+results <- data.frame(results)
+
+ggplot(results, mapping = aes(x = 1:1001, y = results)) +
+  geom_point()
 
 
